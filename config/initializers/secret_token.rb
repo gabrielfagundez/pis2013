61f3da9679787e599b9b90cff3c4c192f0b6a3dd
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-11-06 07:23:05 -0200

    Commit inicial para Azure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PIS2013::Application.config.secret_key_base = 'e3d37d1979844e5641dad6d1eac9ce0c0e19087f3db975a6ec8388d73ccb9d5e9ec2cd2a298e066176b502de3b3122d6a9d100b783dd9dd644ea31b6fb141b18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PIS2013::Application.config.session_store :cookie_store, key: '_PIS2013_session'
